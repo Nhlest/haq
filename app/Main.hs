2574520ef66b5e3671969023230fbd8f1f449509
N`hlest <flutterlice@gmail.com>
2024-03-11 13:18:24 +0200

    Raylib init

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,41 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
-import Lib
+import Raylib.Core
+import Raylib.Util
+import Raylib.Util.Colors
+import Raylib.Core.Textures
+import Raylib.Types.Core
+import Raylib.Types.Core.Textures
+import Graphics.GL.Compatibility30
+import qualified Data.Vector.Storable as SV
+import Foreign hiding (void)
+import Raylib.Util.RLGL (rlCheckErrors)
+import Control.Monad
 
 main :: IO ()
-main = do
-  a <- parseHaQF "hello.haq"
-  print a
-  pure ()
+main = void $ withWindow 800 600 "raylib" 60 startup
+
+startup :: WindowResources -> IO WindowResources
+startup r = do
+  image <- genImageColor 200 200 $ Color 255 0 0 255
+  texture <- loadTextureFromImage image r
+  let v :: SV.Vector Word8 = SV.replicate (200*200*4) 0
+  let v2 :: SV.Vector Word8 = v SV.// concat [[(x*4+3, 255), (x*4+1, 255)]| x <- [0..(200*200-1)]]
+  
+  mainLoop v2 texture False r
+
+mainLoop :: SV.Vector Word8 -> Texture -> Bool -> WindowResources -> IO WindowResources
+mainLoop _ _ True r = pure r
+mainLoop v texture _ r = do
+  let ptr = fst $ SV.unsafeToForeignPtr0 v
+  let t_id = texture'id texture
+  glBindTexture GL_TEXTURE_2D (fromInteger t_id)
+  withForeignPtr ptr $ \p -> do
+    glTexSubImage2D GL_TEXTURE_2D 0 0 0 100 100 GL_RGBA GL_UNSIGNED_BYTE p
+  rlCheckErrors
+  drawing $ do
+    clearBackground lightGray
+    drawTexturePro texture (Rectangle 0.0 0.0 200.0 200.0) (Rectangle 0.0 0.0 800.0 600.0) (Vector2 0.0 0.0) 0.0 white
+  shouldClose <- windowShouldClose
+  mainLoop v texture shouldClose r
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,15 +1,41 @@
 module Lib where
 
 import Text.Parsec.String
-import Text.Parsec (runParser)
+import Text.Parsec
+import Data.Functor
 
-parseHaQF :: String -> IO ()
+parseHaQF :: String -> IO [AST]
 parseHaQF file = do
   text <- readFile file
   case runParser parseHaQ () file text of
-    Left _ -> pure ()
+    Left _ -> error ""
     Right r -> pure r
 
-parseHaQ :: Parser ()
+parseHaQ :: Parser [AST]
 parseHaQ = do
-  pure ()
+  sepBy (choice [plus, number, period]) (many1 $ char ' ') 
+ where number = Number . read <$> many1 digit
+       plus   = char '+' $> Plus
+       period = char '.' $> Period
+
+eval :: [Int] -> [AST] -> ([Int], String)
+eval s [] = (s, mempty)
+eval s (x:xs) = (next_stack, mconcat [out, next_out])
+  where (new_stack, out) = case x of 
+           Number a -> (a:s, "")
+           Plus -> (a+b:rest, "")
+            where (a,b,rest) = pop2 s
+           Period -> (rest, mconcat [show a, "\n"])
+            where (a,rest) = pop s
+        (next_stack, next_out) = eval new_stack xs
+
+data AST = Number Int | Plus | Period deriving Show
+
+pop :: [Int] -> (Int, [Int])
+pop []     = (0, [])
+pop (x:xs) = (x, xs)
+
+pop2 :: [Int] -> (Int, Int, [Int])
+pop2 []       = (0, 0, [])
+pop2 [x]      = (x, 0, [])
+pop2 (x:y:xs) = (x, y, xs)
