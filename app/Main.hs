a03cc2c61c97437f230d5240f86da2ca86973d48
N`hlest <flutterlice@gmail.com>
2024-04-15 20:47:15 +0300

    POC of texture changing

--- app/Main.hs
+++ app/Main.hs
@@ -12,24 +12,34 @@ import qualified Data.Vector.Storable as SV
 import Foreign hiding (void)
 import Raylib.Util.RLGL (rlCheckErrors)
 import Control.Monad
+import Debug.Trace
 
 main :: IO ()
-main = void $ withWindow 800 600 "raylib" 60 startup
+main = do
+  setConfigFlags [VsyncHint]
+  setTargetFPS 0
+  void $ withWindow 800 600 "raylib" 60 startup
 
 startup :: WindowResources -> IO WindowResources
 startup r = do
+  setTargetFPS 0
+
   image <- genImageColor 200 200 $ Color 255 0 0 255
   texture <- loadTextureFromImage image r
   let v :: SV.Vector Word8 = SV.replicate (200*200*4) 0
   let v2 :: SV.Vector Word8 = v SV.// concat [[(x*4+3, 255), (x*4+1, 255)]| x <- [0..(200*200-1)]]
   
-  mainLoop v2 texture False r
+  mainLoop 0 v2 texture False r
 
-mainLoop :: SV.Vector Word8 -> Texture -> Bool -> WindowResources -> IO WindowResources
-mainLoop _ _ True r = pure r
-mainLoop v texture _ r = do
+change :: SV.Vector Word8 -> Int -> SV.Vector Word8
+change v i = trace (show i) $ v SV.// [(i*4+0, 0), (i*4+1, 0), (i*4+2, 0)]
+
+mainLoop :: Int -> SV.Vector Word8 -> Texture -> Bool -> WindowResources -> IO WindowResources
+mainLoop _ _ _ True r = pure r
+mainLoop ii v texture _ r = do
   let ptr = fst $ SV.unsafeToForeignPtr0 v
   let t_id = texture'id texture
+  let v2 = change v ii
   glBindTexture GL_TEXTURE_2D (fromInteger t_id)
   withForeignPtr ptr $ \p -> do
     glTexSubImage2D GL_TEXTURE_2D 0 0 0 100 100 GL_RGBA GL_UNSIGNED_BYTE p
@@ -38,4 +48,4 @@ mainLoop v texture _ r = do
     clearBackground lightGray
     drawTexturePro texture (Rectangle 0.0 0.0 200.0 200.0) (Rectangle 0.0 0.0 800.0 600.0) (Vector2 0.0 0.0) 0.0 white
   shouldClose <- windowShouldClose
-  mainLoop v texture shouldClose r
+  mainLoop (ii+1) v2 texture shouldClose r
