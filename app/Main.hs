4bc806ec0423526d7e0294de1dfd5b508792ed30
N`hlest <flutterlice@gmail.com>
2024-04-20 03:12:23 +0300

    Parsing

--- app/Main.hs
+++ app/Main.hs
@@ -14,24 +14,42 @@ import Foreign hiding (void)
 import Raylib.Util.RLGL (rlCheckErrors)
 import Control.Monad
 
+import Lib
+
 main :: IO ()
 main = do
-  setConfigFlags [VsyncHint]
-  setTargetFPS 0
-  void $ withWindow 800 600 "raylib" 60 startup
+  ast <- parseHaQF "hello.haq"
+  print ast
+  -- setConfigFlags [VsyncHint]
+  -- setTargetFPS 0
+  -- void $ withWindow 800 600 "raylib" 60 startup
 
 startup :: WindowResources -> IO WindowResources
 startup r = do
   setTargetFPS 0
 
-  image <- genImageColor 200 200 $ Color 255 0 0 255
-  texture <- loadTextureFromImage image r
-  let v :: SV.Vector Word8 = SV.replicate (200*200*4) 0
-  let v2 :: SV.Vector Word8 = v SV.// concat [[(x*4+3, 255), (x*4+1, 255)]| x <- [0..(200*200-1)]]
+  glEnable GL_BLEND
+  glBlendFunc GL_SRC_ALPHA GL_ONE_MINUS_SRC_ALPHA
 
-  mv <- SV.thaw v2
+  image <- genImageColor 200 200 $ Color 255 0 0 255
+  n_image <- imageFormat image PixelFormatUncompressedR5G5B5A1
+  texture <- loadTextureFromImage n_image r
+  let v :: SV.Vector Word8 = SV.replicate (200*200*2) 0
+
+  mv <- SV.thaw v
   
   mainLoop 0 mv texture False r
+
+changeColor :: Ptr Word8 -> Int -> (Int, Int, Int, Int) -> IO ()
+changeColor ptr i (r, g, b, a) = do
+    let p1 :: Ptr Word8 = plusPtr ptr (i*2)
+    let p2 :: Ptr Word8 = plusPtr ptr (i*2 + 1)
+    let c = a * 32768 + b * 1024 + g * 32 + r
+    let (b1, b2) = divMod c 256 
+    -- poke p1 (fromIntegral b1)
+    -- poke p2 (fromIntegral b2)
+    poke p1 31
+    poke p2 128
 
 mainLoop :: Int -> MV.IOVector Word8 -> Texture -> Bool -> WindowResources -> IO WindowResources
 mainLoop _ _ _ True r = pure r
@@ -39,12 +57,11 @@ mainLoop ii v texture _ r = do
   MV.unsafeWith v $ \ptr -> do
     let t_id = texture'id texture
     glBindTexture GL_TEXTURE_2D (fromInteger t_id)
-    glTexSubImage2D GL_TEXTURE_2D 0 0 0 100 100 GL_RGBA GL_UNSIGNED_BYTE ptr
+    glTexSubImage2D GL_TEXTURE_2D 0 0 0 200 200 GL_RGBA GL_UNSIGNED_BYTE ptr
     rlCheckErrors
     drawing $ do
       clearBackground lightGray
       drawTexturePro texture (Rectangle 0.0 0.0 200.0 200.0) (Rectangle 0.0 0.0 800.0 600.0) (Vector2 0.0 0.0) 0.0 white
-    let np :: Ptr Word8 = plusPtr ptr (ii * 4 + 1)
-    poke np 0
+    changeColor ptr ii (31, 0, 0, 31)
   shouldClose <- windowShouldClose
   mainLoop (ii+1) v texture shouldClose r
--- src/Lib.hs
+++ src/Lib.hs
@@ -3,39 +3,83 @@ module Lib where
 import Text.Parsec.String
 import Text.Parsec
 import Data.Functor
+import Control.Applicative
 
 parseHaQF :: String -> IO [AST]
 parseHaQF file = do
   text <- readFile file
   case runParser parseHaQ () file text of
-    Left _ -> error ""
+    Left e -> error $ show e
     Right r -> pure r
 
 parseHaQ :: Parser [AST]
 parseHaQ = do
-  sepBy (choice [plus, number, period]) (many1 $ char ' ') 
- where number = Number . read <$> many1 digit
-       plus   = char '+' $> Plus
-       period = char '.' $> Period
+  block <- parseASTs
+  eof
+  pure block
 
-eval :: [Int] -> [AST] -> ([Int], String)
-eval s [] = (s, mempty)
-eval s (x:xs) = (next_stack, mconcat [out, next_out])
-  where (new_stack, out) = case x of 
-           Number a -> (a:s, "")
-           Plus -> (a+b:rest, "")
-            where (a,b,rest) = pop2 s
-           Period -> (rest, mconcat [show a, "\n"])
-            where (a,rest) = pop s
-        (next_stack, next_out) = eval new_stack xs
+parseASTs :: Parser [AST]
+parseASTs = do 
+  sepEndBy parseAST whitespace
 
-data AST = Number Int | Plus | Period deriving Show
+parseAST :: Parser AST
+parseAST = do
+  choice [try block, try block1, stringlit, try glyphedword, try biglyph, try glyph, try number, try word]
+ where 
+       number = Number . read <$> many1 digit
+       word        = do 
+        g <- many1 alphabet
+        void $ lookAhead $ noneOf "}])"
+        pure $ Word g
+       glyph       = do 
+        g <- glyph_
+        void $ lookAhead $ noneOf "}])"
+        pure $ Glyph g
+       alphabet    = oneOf "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+       glyph_      = oneOf "':/+.->"
+       biglyph     = liftA2 BiGlyph glyph_ glyph_
+       glyphedword = do
+        g <- glyph_
+        word_ <- many1 alphabet
+        pure $ GlyphedWord g word_
+       stringlit = do
+        lit <- between (char '"') (char '"') (many1 alphaNum)
+        pure $ StringLit lit
+       block1 = do
+        bracket <- lookAhead $ try $ oneOf "([{"
+        b <- between (sequence [string [bracket], whitespace]) (sequence [string [reverseBracket bracket]]) parseASTs
+        pure $ Block_ (bracketType bracket) b
+       block = do
+        (bracket, ch) <- lookAhead $ do
+          c1 <- try $ oneOf "([{"
+          c2 <- anyChar
+          pure (c1, c2)
+        b <- between (sequence [string [bracket, ch], whitespace]) (sequence [string [ch, reverseBracket bracket]]) parseASTs
+        pure $ Block (bracketType bracket) (GlyphC ch) b
 
-pop :: [Int] -> (Int, [Int])
-pop []     = (0, [])
-pop (x:xs) = (x, xs)
+reverseBracket :: Char -> Char
+reverseBracket '(' = ')'
+reverseBracket '[' = ']'
+reverseBracket '{' = '}'
+reverseBracket _   = error "Unknown bracket"
+bracketType :: Char -> BracketType
+bracketType '(' = Round
+bracketType '[' = Square
+bracketType '{' = Squigly
+bracketType _   = error "Unknown bracket"
 
-pop2 :: [Int] -> (Int, Int, [Int])
-pop2 []       = (0, 0, [])
-pop2 [x]      = (x, 0, [])
-pop2 (x:y:xs) = (x, y, xs)
+whitespace :: Parser String
+whitespace = many1 $ oneOf " \n\t"
+
+data BracketType = Round | Square | Squigly deriving Show
+newtype GlyphC = GlyphC Char deriving Show
+
+data AST = Number Int 
+         | Glyph Char
+         | BiGlyph Char Char
+         | Word String
+         | GlyphedWord Char String
+         | StringLit String
+         | Block BracketType GlyphC [AST]
+         | Block_ BracketType [AST]
+         deriving Show
